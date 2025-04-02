75861801608e698bd5853c8c2f4bf90b37f12c61
Emre Özcan <67864140+eoezcan2@users.noreply.github.com>
2025-04-02 15:58:48 +0200

    Ready to test on server

--- otp_service.py
+++ otp_service.py
@@ -3,6 +3,7 @@ import uuid
 import smpplib.client
 import smpplib.consts
 import requests
+from requests.auth import HTTPBasicAuth
 import datetime
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -22,7 +23,9 @@ DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_NAME = os.getenv('DB_NAME', 'otp_db')
 
 # Database connection string
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
+
+print(DATABASE_URL)
 
 # Set up SQLAlchemy
 engine = create_engine(DATABASE_URL)
@@ -38,7 +41,8 @@ class OTP(Base):
     provider = Column(String(50))
     phone_number = Column(String(15), index=True)
     text = Column(String(255))
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    status = Column(String(50), default="pending")
+    created_at = Column(DateTime, default=datetime.datetime.now)
 
 # Create the database tables and update the schema if needed
 Base.metadata.create_all(bind=engine)
@@ -48,11 +52,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Allow only your frontend
+    allow_origins=["http://localhost:5173", "http://localhost:80", "http://localhost"],  # Allow only your frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
+
+sid = os.getenv('SMS_SERVICE_API_KEY')
+auth_token = os.getenv('SMS_SERVICE_API_SECRET')
 
 # POST Endpoint to send OTP
 class OTPRequest(BaseModel):
@@ -63,21 +70,25 @@ class OTPRequest(BaseModel):
 @app.post("/send_otp/")
 async def send_otp(otp_request: OTPRequest):
     # Send the OTP via HTTP API Request
-    request_url = f"https://rest.nexmo.com/sms/json"
+    request_url = f'https://api.twilio.com/2010-04-01/Accounts/{sid}/Messages.json'
     request_body = {
-        "api_key": os.getenv('NEXMO_API_KEY'),
-        "api_secret": os.getenv('NEXMO_API_SECRET'),
-        "from": os.getenv('NEXMO_FROM'),
-        "to": otp_request.phone_number,
-        "text": otp_request.text
+        "From": os.getenv('SMS_SERVICE_FROM'),
+        "To": otp_request.phone_number,
+        "Body": otp_request.text
     }
-    response = requests.post(request_url, data=request_body)
+    response = requests.post(request_url, data=request_body, auth=HTTPBasicAuth(sid, auth_token))
     response_json = response.json()
     print(response_json)
 
     # Save the OTP in the database
     db = SessionLocal()
     db_otp = OTP(phone_number=otp_request.phone_number, provider=otp_request.provider, text=otp_request.text)
+
+    if response_json.get('error_code'):
+        db_otp.status = "failed"
+    else:
+        db_otp.status = "sent"
+
     db.add(db_otp)
     db.commit()
     db.refresh(db_otp)
@@ -93,7 +104,3 @@ async def get_otp():
     db.close()
 
     return otps
-
-def generate_otp():
-    otp = str(uuid.uuid4().int)[:6]
-    return otp
